7fb6bfab4fba7c750febe186adaf3b8bc0d5967a
효닝 <gus5686@naver.com>
2021-07-21 18:57:37 +0900

    검색어페이지 숫자까지는 일단락

--- Art/src/main/java/com/gdj35/art/web/user/controller/SearchController.java
+++ Art/src/main/java/com/gdj35/art/web/user/controller/SearchController.java
@@ -74,7 +74,7 @@ public class SearchController {
 			
 			PagingBean pb = iPagingService.getPagingBean(page, cnt, 12, 5);
 			
-		
+			System.out.println("***********사진파람: " + params);
 			params.put("startCnt", Integer.toString(pb.getStartCount()));
 			params.put("endCnt", Integer.toString(pb.getEndCount()));
 					
@@ -104,7 +104,7 @@ public class SearchController {
 			int cnt = iSearchService.getSCnt(params);
 			
 			PagingBean pb = iPagingService.getPagingBean(page, cnt, 12, 5);
-			
+			System.out.println("***********그림파람: " + params);
 		
 			params.put("startCnt", Integer.toString(pb.getStartCount()));
 			params.put("endCnt", Integer.toString(pb.getEndCount()));
@@ -135,7 +135,7 @@ public class SearchController {
 			int cnt = iSearchService.getSCnt(params);
 			
 			PagingBean pb = iPagingService.getPagingBean(page, cnt, 12, 5);
-			
+			System.out.println("***********동영상파람: " + params);
 		
 			params.put("startCnt", Integer.toString(pb.getStartCount()));
 			params.put("endCnt", Integer.toString(pb.getEndCount()));
@@ -162,15 +162,14 @@ public class SearchController {
 			Map<String, Object> modelMap = new HashMap<String, Object>();
 			
 			int page = Integer.parseInt(params.get("page"));
-
 			int cnt = iSearchService.getWCnt(params);
 			
 			PagingBean pb = iPagingService.getPagingBean(page, cnt, 12, 5);
 			
-		
+			System.out.println("***********작가파람: " + params);
 			params.put("startCnt", Integer.toString(pb.getStartCount()));
 			params.put("endCnt", Integer.toString(pb.getEndCount()));
-					
+
 			List<HashMap<String, String>> list = iSearchService.getWriterList(params);
 			
 			modelMap.put("cnt", cnt);
